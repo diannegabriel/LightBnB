b2dddbed006e0c8313ce16e3584192f6cb605835
Dianne <mdianne.gabriel@gmail.com>
2021-08-18 15:33:29 -0400

    Add conditional for minimum and maximum price per night

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -119,6 +119,24 @@ const getAllProperties = (options, limit = 10) => {
     }
   }
 
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night}`)
+    if (queryParams.length > 1) {
+      queryString += `AND cost_per_night >= $${queryParams.length}`;
+    } else {
+      queryString += `AND cost_per_night >= $${queryParams.length}`;
+    }
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night}`)
+    if (queryParams.length > 1) {
+      queryString += `AND cost_per_night <= $${queryParams.length}`;
+    } else {
+      queryString += `AND cost_per_night <= $${queryParams.length}`;
+    }
+  }
+
   queryParams.push(limit);
   queryString += `
   GROUP BY properties.id
