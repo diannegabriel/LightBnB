7f2bd525ec507f8fa9360b54a4324378b06dbe62
Dianne <mdianne.gabriel@gmail.com>
2021-08-18 14:33:09 -0400

    Refactor getAllProperties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -73,7 +73,7 @@ const getAllReservations = function(guest_id, limit = 10) {
     WHERE reservations.guest_id = $1
     AND reservations.end_date < now()::date
     GROUP BY properties.id, reservations.id
-    ORDER BY reservations.start_date
+    ORDER BY reservations.start_date DESC
     LIMIT $2;`, [guest_id, limit])
     .then(result => result.rows)
     .catch((err) => {
@@ -91,12 +91,40 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then(result => result.rows)
-    .catch((err) => {
-      console.log(err.message);
-    });
+  // return pool
+  //   .query(`SELECT * FROM properties LIMIT $1`, [limit])
+  //   .then(result => result.rows)
+  //   .catch((err) => {
+  //     console.log(err.message);
+  //   });
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 };
   
 exports.getAllProperties = getAllProperties;
