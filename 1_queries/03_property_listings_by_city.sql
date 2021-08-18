98737a48085dff809ddd804eea4c38be496d3815
Dianne <mdianne.gabriel@gmail.com>
2021-08-17 20:44:59 -0400

    Fix mistake in 03_property_listings_by_city.sql

--- 1_queries/03_property_listings_by_city.sql
+++ 1_queries/03_property_listings_by_city.sql
@@ -1,6 +1,6 @@
 -- Show all details about properties located in Vancouver including their average rating.
 
-SELECT properties.id, title, cost_per_night, AVG(property_reviews.rating) AS average_rating
+SELECT properties.*, AVG(property_reviews.rating) AS average_rating
 FROM properties
 JOIN property_reviews ON properties.id = property_id
 WHERE city LIKE '%Vancouver%'
