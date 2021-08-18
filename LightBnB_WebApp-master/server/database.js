25b19fa8165eb41cc89b34df0851c3b467ab546f
Dianne <mdianne.gabriel@gmail.com>
2021-08-18 13:55:52 -0400

    Update getAllReservations so it only returns listing that user booked in the past

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -35,7 +35,8 @@ const getUserWithId = function(id) {
     .then(result => result.rows[0])
     .catch((err) => {
       console.log(err.message);
-    });}
+  });
+}
 exports.getUserWithId = getUserWithId;
 
 
@@ -63,7 +64,21 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`
+    SELECT properties.*, reservations.*, AVG(rating) AS average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id 
+    WHERE reservations.guest_id = $1
+    AND reservations.end_date < now()::date
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2;`, [guest_id, limit])
+    .then(result => result.rows)
+    .catch((err) => {
+      console.log(err.message);
+  });
 }
 exports.getAllReservations = getAllReservations;
 
