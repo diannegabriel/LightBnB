14dafd41b0cc2ed4828f118df0434950bf6d557d
Dianne <mdianne.gabriel@gmail.com>
2021-08-17 21:09:29 -0400

    Update 05_all_user_reservations.sql with Compass answer

--- 1_queries/05_all_user_reservations.sql
+++ 1_queries/05_all_user_reservations.sql
@@ -9,3 +9,14 @@ WHERE reservations.guest_id = 1
 GROUP BY properties.id, reservations.id
 ORDER BY reservations.start_date ASC
 LIMIT 10;
+
+-- Compass answer:
+-- SELECT reservations.*, properties.*, AVG(rating) AS average_rating
+-- FROM reservations
+-- JOIN properties ON reservations.property_id = properties.id
+-- JOIN property_reviews ON properties.id = property_reviews.property_id
+-- WHERE reservations.guest_id = 1
+-- AND end_date < NOW()::DATE
+-- GROUP BY properties.id, reservations.id
+-- ORDER BY reservations.start_date
+-- LIMIT 10; 
