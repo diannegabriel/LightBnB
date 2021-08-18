66c270bd066cdd1adea7ee052b3054052675dc67
Dianne <mdianne.gabriel@gmail.com>
2021-08-17 22:30:17 -0400

    Update 01_seeds.sql. Remove id from each table

--- 1_queries/05_all_user_reservations.sql
+++ 1_queries/05_all_user_reservations.sql
@@ -1,22 +1,22 @@
 -- Show all reservations for a user.
 
-SELECT properties.id, properties.title, properties.cost_per_night, reservations.start_date, AVG(rating) AS average_rating
-FROM property_reviews
-JOIN properties ON properties.id = property_id
-JOIN reservations ON reservations.id = reservation_id
-WHERE reservations.guest_id = 1
-  AND reservations.end_date < NOW()::DATE
-GROUP BY properties.id, reservations.id
-ORDER BY reservations.start_date ASC
-LIMIT 10;
+-- SELECT properties.id, properties.title, properties.cost_per_night, reservations.start_date, AVG(rating) AS average_rating
+-- FROM property_reviews
+-- JOIN properties ON properties.id = property_id
+-- JOIN reservations ON reservations.id = reservation_id
+-- WHERE reservations.guest_id = 1
+--   AND reservations.end_date < NOW()::DATE
+-- GROUP BY properties.id, reservations.id
+-- ORDER BY reservations.start_date ASC
+-- LIMIT 10;
 
 -- Compass answer:
--- SELECT reservations.*, properties.*, AVG(rating) AS average_rating
--- FROM reservations
--- JOIN properties ON reservations.property_id = properties.id
--- JOIN property_reviews ON properties.id = property_reviews.property_id
--- WHERE reservations.guest_id = 1
--- AND end_date < NOW()::DATE
--- GROUP BY properties.id, reservations.id
--- ORDER BY reservations.start_date
--- LIMIT 10; 
+SELECT reservations.*, properties.*, AVG(rating) AS average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+AND end_date < NOW()::DATE
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT 10; 
